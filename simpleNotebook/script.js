79815a9e8f032f8ac01a99d0a818bd7db7a9f80e
Saber Heydari <Silverethical@gmail.com>
2022-06-15 16:57:04 +0430

    add/remove notes from page/LS

--- simpleNotebook/script.js
+++ simpleNotebook/script.js
@@ -1,12 +1,48 @@
 // selectors
-let htmlInputForm = document.querySelector("#input-form");
-let htmlNoteList = document.querySelector("#storage");
+let htmlInputForm = document.querySelector("#input-form"),
+  htmlNoteList = document.querySelector("#storage");
 
-// Event Listeners
-htmlInputForm.addEventListener("submit", addNote);
+// array of all saved notes
+let notesArray = [];
 
-// Functions
-function addNote(e) {
+// check if there are notes from before
+if (!!localStorage.getItem("notes")) {
+  notesArray = JSON.parse(localStorage.getItem("notes"));
+
+  for (let i = 0; i < notesArray.length; i++) {
+    addNoteToPage(notesArray[i]);
+  }
+}
+
+function addNoteToPage(noteText) {
+  // create removeBtn inside <div>
+  const newRemoveDiv = document.createElement("div");
+  const newRemoveBtn = document.createElement("a");
+  newRemoveBtn.textContent = "x";
+  newRemoveBtn.classList.add("remove-btn");
+  newRemoveBtn.addEventListener("click", removeNote);
+  newRemoveDiv.appendChild(newRemoveBtn);
+
+  // create note <p> inside <div>
+  let newNoteDiv = document.createElement("div");
+  let newNoteP = document.createElement("p");
+  newNoteP.appendChild(document.createTextNode(noteText));
+  newNoteDiv.appendChild(newNoteP);
+
+  // place note and removeBtn in a container
+  const newNoteContainer = document.createElement("div");
+  newNoteContainer.classList.add("notes");
+  let htmlNotesDiv = document.querySelectorAll(".notes");
+  newNoteContainer.setAttribute("id", htmlNotesDiv.length + 1);
+  newNoteContainer.appendChild(newNoteDiv);
+  newNoteContainer.appendChild(newRemoveBtn);
+
+  // add the container to the page
+  htmlNoteList.appendChild(newNoteContainer);
+}
+
+// add note to page
+function addNewNote(e) {
   e.preventDefault();
 
   let htmlNoteValue = document.querySelector("#note").value;
@@ -14,29 +50,36 @@ function addNote(e) {
     // if there is no text
     alert("یادداشت خالی!");
   } else {
-    // create note <p>
-    const newNoteDiv = document.createElement("div");
-    const newNoteP = document.createElement("p");
-    newNoteP.appendChild(document.createTextNode(htmlNoteValue));
-    newNoteDiv.appendChild(newNoteP);
+    // add new note to page
+    addNoteToPage(htmlNoteValue);
 
-    // create removeBtn
-    const newRemoveDiv = document.createElement("div");
-    const newRemoveBtn = document.createElement("a");
-    newRemoveBtn.textContent = "x";
-    newRemoveBtn.classList.add("remove-btn");
-    newRemoveDiv.appendChild(newRemoveBtn);
+    // add new note to LocalStorage
+    notesArray.push(htmlNoteValue);
+    localStorage.setItem("notes", JSON.stringify(notesArray));
 
-    // place note and removeBtn in a container
-    const newNoteContainer = document.createElement("div");
-    newNoteContainer.classList.add("notes");
-    newNoteContainer.appendChild(newNoteDiv);
-    newNoteContainer.appendChild(newRemoveDiv);
-
-    // add the container to the page
-    htmlNoteList.appendChild(newNoteContainer);
+    // reset the form
+    this.reset();
   }
-
-  // reset the form
-  this.reset();
 }
+
+function removeNote(e) {
+  // clicked note's index in notesArray
+  let noteIndex = e.path[1].id - 1;
+  // remove note from array
+  notesArray.splice(noteIndex, 1);
+
+  // remove note from page
+  e.path[1].remove();
+
+  // adjust LocalStorage
+  localStorage.setItem("notes", JSON.stringify(notesArray));
+
+  // adjust id of .notes
+  let htmlNotesDiv = document.querySelectorAll(".notes");
+  for (let i = 0; i < htmlNotesDiv.length; i++) {
+    htmlNotesDiv[i].setAttribute("id", i + 1);
+  }
+}
+
+// Event Listeners
+htmlInputForm.addEventListener("submit", addNewNote);
