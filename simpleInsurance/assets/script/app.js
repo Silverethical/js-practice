7844a08d5b66656f2c422ddf8c0d032b7e3b995b
Saber Heydari <Silverethical@gmail.com>
2022-07-24 12:22:41 +0430

    automatically generate shamsi years, minor visual changes

--- simpleInsurance/assets/script/app.js
+++ simpleInsurance/assets/script/app.js
@@ -1,1 +1,25 @@
 "use strict";
+
+document.addEventListener("DOMContentLoaded", appInit);
+
+function appInit() {
+  const selectYear = document.querySelector("#select-car-year"),
+    newDate = new Date(),
+    // convert year to shamsi
+    yearShamsi = Number(
+      new Intl.DateTimeFormat("en-US-u-ca-persian", {
+        year: "numeric",
+      })
+        .format(newDate)
+        .replace(" AP", "")
+    ),
+    max = yearShamsi,
+    min = max - 20;
+
+  for (let i = max; i >= min; i--) {
+    let newOption = document.createElement("option");
+    newOption.value = i;
+    newOption.innerHTML = i;
+    selectYear.appendChild(newOption);
+  }
+}
