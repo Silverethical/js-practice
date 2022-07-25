06c72327facccc1ea1cef8f48ace20089b2a185f
Saber Heydari <Silverethical@gmail.com>
2022-07-25 17:15:36 +0430

    making things better and more clear

--- simpleInsurance/assets/script/app.js
+++ simpleInsurance/assets/script/app.js
@@ -1,25 +1,53 @@
 "use strict";
 
+// variables
+let basePrice = 2_000_000,
+  basicPlanRatio = 1,
+  fullPlanRatio = 2;
+
+// event listeners
 document.addEventListener("DOMContentLoaded", appInit);
 
 function appInit() {
-  const selectYear = document.querySelector("#select-car-year"),
-    newDate = new Date(),
-    // convert year to shamsi
-    yearShamsi = Number(
-      new Intl.DateTimeFormat("en-US-u-ca-persian", {
-        year: "numeric",
-      })
-        .format(newDate)
-        .replace(" AP", "")
-    ),
-    max = yearShamsi,
-    min = max - 20;
+  generateYears();
+}
 
-  for (let i = max; i >= min; i--) {
-    let newOption = document.createElement("option");
-    newOption.value = i;
-    newOption.innerHTML = i;
-    selectYear.appendChild(newOption);
+function getYearShamsi(targetDate = new Date()) {
+  if (typeof targetDate.getFullYear() == "number") {
+    // convert year to shamsi
+    const yearShamsi = new Intl.DateTimeFormat("en-US-u-ca-persian", {
+        year: "numeric",
+      }).format(targetDate),
+      // remove extra string and convert to number
+      result = Number(yearShamsi.replace(" AP", ""));
+
+    return result;
   }
 }
+
+function generateYears(maxYear = getYearShamsi(), minYear = maxYear - 20) {
+  if (maxYear > minYear) {
+    const selectYear = document.querySelector("#select-car-year");
+    for (let i = maxYear; i >= minYear; i--) {
+      let newOption = document.createElement("option");
+      newOption.value = i;
+      newOption.innerHTML = i;
+      selectYear.appendChild(newOption);
+    }
+  }
+}
+
+function NewCar(carModel, ratio) {
+  (this.carModel = carModel),
+    (this.ratio = ratio),
+    (calcPrice = function () {});
+}
+
+function newInsurancePlan(planName, planRatio) {
+  (this.planName = planName), (this.planRatio = planRatio);
+}
+
+let test = {
+  basicPlan: { ratio: 2 },
+  fullPlan: { ratio: 4 },
+};
