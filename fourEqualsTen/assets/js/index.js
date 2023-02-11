56e14c27b37e3cbf70e9ec31b87617dedf90ccc0
Saber Heydari <silverethical@gmail.com>
2023-02-11 20:12:35 +0330

    feat(fourEqualsTen): add basic html inputs so we can interact

--- fourEqualsTen/assets/js/index.js
+++ fourEqualsTen/assets/js/index.js
@@ -1,3 +1,11 @@
+/** selectors */
+const allInputs = document.querySelectorAll("input"),
+  numbersInput = document.querySelector("#numbers"),
+  goalInput = document.querySelector("#goal"),
+  findSolutionsButton = document.querySelector("#findSolutionsButton"),
+  answersDiv = document.querySelector("#answers"),
+  numberOfSolutions = document.querySelector(".numberOfSolutions");
+
 const operators = ["+", "-", "*", "/"],
   allPossibleOperations = arrayCreate(operators, 3);
 
@@ -17,8 +25,17 @@ for (const operator of operators) {
 /** initialize result for later to store solutions in */
 let result = [];
 
-/** calculate the solutions */
-calculateSolutions([3, 1, 8, 5], 10);
+/** when document is ready */
+document.addEventListener("DOMContentLoaded", () => {
+  for (const input of allInputs) {
+    input.addEventListener("input", validation);
+  }
+
+  findSolutionsButton.addEventListener("click", () => {
+    /** calculate the solutions */
+    calculateSolutions(numbersInput.value.split(""), goalInput.value);
+  });
+});
 
 /**
  * return all possible combination of an array
@@ -49,7 +66,7 @@ function arrayCreate(array, size) {
 /**
  * calculate all the solutions to get to desired number with 4 numbers
  * @param {object} numbers - an array of numbers
- * @returns 
+ * @returns
  */
 function calculateSolutions(numbers, goal) {
   try {
@@ -132,7 +149,7 @@ function calculateSolutions(numbers, goal) {
         /** loop over the calculation list */
         for (const calculation of calculationList) {
           /** if calculation equals the desired number */
-          if (eval(calculation) === goal) {
+          if (eval(calculation) === Number(goal)) {
             /** push the calculation to the result array */
             result.push(calculation);
           }
@@ -141,19 +158,44 @@ function calculateSolutions(numbers, goal) {
     }
 
     /** log the number of solutions */
-    console.log(`There are ${result.length} solutions.`);
+    // console.log(`There are ${result.length} solutions.`);
 
     /** log the solutions */
-    console.log("Solutions: ", result);
+    // console.log("Solutions: ", result);
 
     /** end measuring time */
     let timeTaken = performance.now() - start;
 
     /** log time taken to calculate all these */
     console.log("Total time taken : " + timeTaken + " milliseconds");
+
+    /** print solutions in page */
+    printResult(result)
   } catch (error) {
     /** log error */
     console.error(error);
     return false;
+  }
+}
+
+function validation() {
+  if (
+    Number(numbersInput.value) &&
+    Number(goalInput.value) &&
+    numbersInput.value.length === 4 &&
+    goalInput.value.length <= 2
+  ) {
+    findSolutionsButton.disabled = false;
+  } else {
+    findSolutionsButton.disabled = true;
+  }
+}
+
+function printResult(solutions) {
+  answersDiv.innerHTML = "";
+  numberOfSolutions.textContent = `(${solutions.length})`;
+  
+  for (const solution of solutions) {
+    answersDiv.innerHTML += `<p>${solution}</p>`
   }
 }
