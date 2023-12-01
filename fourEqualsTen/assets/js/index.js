23622581d95b87f144e0016a20b80f3001617eb3
Saber Heydari <Silverethical@gmail.com>
2023-12-01 14:08:14 +0330

    feat(fourEqualsTen): I don; remember what this is
    Description:
    This is for +8 months ago and I have no idea what is going on. I'm just committing so it is not lost. :)

--- fourEqualsTen/assets/js/index.js
+++ fourEqualsTen/assets/js/index.js
@@ -1,26 +1,26 @@
 /** selectors */
 const allInputs = document.querySelectorAll("input"),
-  numbersInput = document.querySelector("#numbers"),
-  goalInput = document.querySelector("#goal"),
-  findSolutionsButton = document.querySelector("#findSolutionsButton"),
-  answersDiv = document.querySelector("#answers"),
-  numberOfSolutions = document.querySelector(".numberOfSolutions");
+	numbersInput = document.querySelector("#numbers"),
+	goalInput = document.querySelector("#goal"),
+	findSolutionsButton = document.querySelector("#findSolutionsButton"),
+	answersDiv = document.querySelector("#answers"),
+	numberOfSolutions = document.querySelector(".numberOfSolutions");
 
 const operators = ["+", "-", "*", "/"],
-  allPossibleOperations = arrayCreate(operators, 3);
+	allPossibleOperations = arrayCreate(operators, 3);
 
 /** add additional operators where there are more than one instance of the one operator */
 for (const operator of operators) {
-  allPossibleOperations.push(
-    [operator, operator, "+"],
-    [operator, operator, "-"],
-    [operator, operator, "*"],
-    [operator, operator, "/"],
-    [operator, "+", "+"],
-    [operator, "-", "-"],
-    [operator, "*", "*"],
-    [operator, "/", "/"]
-  );
+	allPossibleOperations.push(
+		[operator, operator, "+"],
+		[operator, operator, "-"],
+		[operator, operator, "*"],
+		[operator, operator, "/"],
+		[operator, "+", "+"],
+		[operator, "-", "-"],
+		[operator, "*", "*"],
+		[operator, "/", "/"]
+	);
 }
 
 /** initialize result for later to store solutions in */
@@ -28,14 +28,14 @@ let result = [];
 
 /** when document is ready */
 document.addEventListener("DOMContentLoaded", () => {
-  for (const input of allInputs) {
-    input.addEventListener("input", validation);
-  }
+	for (const input of allInputs) {
+		input.addEventListener("input", validation);
+	}
 
-  findSolutionsButton.addEventListener("click", () => {
-    /** calculate the solutions */
-    calculateSolutions(numbersInput.value.split(""), goalInput.value);
-  });
+	findSolutionsButton.addEventListener("click", () => {
+		/** calculate the solutions */
+		calculateSolutions(numbersInput.value.split(""), goalInput.value);
+	});
 });
 
 /**
@@ -45,23 +45,23 @@ document.addEventListener("DOMContentLoaded", () => {
  * @returns
  */
 function arrayCreate(array, size) {
-  let newArray = [];
-  array.forEach(
-    (function iter(parts) {
-      return function (v) {
-        let temp = parts.concat(v);
-        if (parts.includes(v)) {
-          return;
-        }
-        if (temp.length === size) {
-          newArray.push(temp);
-          return;
-        }
-        array.forEach(iter(temp));
-      };
-    })([])
-  );
-  return newArray;
+	let newArray = [];
+	array.forEach(
+		(function iter(parts) {
+			return function (v) {
+				let temp = parts.concat(v);
+				if (parts.includes(v)) {
+					return;
+				}
+				if (temp.length === size) {
+					newArray.push(temp);
+					return;
+				}
+				array.forEach(iter(temp));
+			};
+		})([])
+	);
+	return newArray;
 }
 
 /**
@@ -70,136 +70,136 @@ function arrayCreate(array, size) {
  * @returns
  */
 function calculateSolutions(numbers, goal) {
-  try {
-    /** check if the numbers parameter is an array with 4 indexes */
-    if (!Array.isArray(numbers) || numbers.length != 4)
-      /** throw an error */
-      throw new Error("Please specify an array with 4 numbers");
+	try {
+		/** check if the numbers parameter is an array with 4 indexes */
+		if (!Array.isArray(numbers) || numbers.length != 4)
+			/** throw an error */
+			throw new Error("Please specify an array with 4 numbers");
 
-    /** remove previously calculated result */
-    result = [];
+		/** remove previously calculated result */
+		result = [];
 
-    /** start measuring time */
-    let start = performance.now();
+		/** start measuring time */
+		let start = performance.now();
 
-    /** create all possible combination of the given numbers */
-    const allNumCombinations = arrayCreate(numbers, 4);
+		/** create all possible combination of the given numbers */
+		const allNumCombinations = arrayCreate(numbers, 4);
 
-    /** loop over all the possible combination of numbers */
-    for (const numCombinations of allNumCombinations) {
-      /** loop over all the possible operations */
-      for (const possibleOperations of allPossibleOperations) {
-        /** make a calculation list with parentheses in different parts */
-        const calculationList = [
-          "(" +
-            numCombinations[0] +
-            possibleOperations[0] +
-            numCombinations[1] +
-            possibleOperations[1] +
-            numCombinations[2] +
-            possibleOperations[2] +
-            numCombinations[3] +
-            ")",
-          "(" +
-            numCombinations[0] +
-            possibleOperations[0] +
-            numCombinations[1] +
-            possibleOperations[1] +
-            numCombinations[2] +
-            ")" +
-            possibleOperations[2] +
-            numCombinations[3],
-          "(" +
-            numCombinations[0] +
-            possibleOperations[0] +
-            numCombinations[1] +
-            ")" +
-            possibleOperations[1] +
-            numCombinations[2] +
-            possibleOperations[2] +
-            numCombinations[3],
-          numCombinations[0] +
-            possibleOperations[0] +
-            "(" +
-            numCombinations[1] +
-            possibleOperations[1] +
-            numCombinations[2] +
-            possibleOperations[2] +
-            numCombinations[3] +
-            ")",
-          numCombinations[0] +
-            possibleOperations[0] +
-            numCombinations[1] +
-            possibleOperations[1] +
-            "(" +
-            numCombinations[2] +
-            possibleOperations[2] +
-            numCombinations[3] +
-            ")",
-          numCombinations[0] +
-            possibleOperations[0] +
-            "(" +
-            numCombinations[1] +
-            possibleOperations[1] +
-            numCombinations[2] +
-            ")" +
-            possibleOperations[2] +
-            numCombinations[3],
-        ];
+		/** loop over all the possible combination of numbers */
+		for (const numCombinations of allNumCombinations) {
+			/** loop over all the possible operations */
+			for (const possibleOperations of allPossibleOperations) {
+				/** make a calculation list with parentheses in different parts */
+				const calculationList = [
+					"(" +
+						numCombinations[0] +
+						possibleOperations[0] +
+						numCombinations[1] +
+						possibleOperations[1] +
+						numCombinations[2] +
+						possibleOperations[2] +
+						numCombinations[3] +
+						")",
+					"(" +
+						numCombinations[0] +
+						possibleOperations[0] +
+						numCombinations[1] +
+						possibleOperations[1] +
+						numCombinations[2] +
+						")" +
+						possibleOperations[2] +
+						numCombinations[3],
+					"(" +
+						numCombinations[0] +
+						possibleOperations[0] +
+						numCombinations[1] +
+						")" +
+						possibleOperations[1] +
+						numCombinations[2] +
+						possibleOperations[2] +
+						numCombinations[3],
+					numCombinations[0] +
+						possibleOperations[0] +
+						"(" +
+						numCombinations[1] +
+						possibleOperations[1] +
+						numCombinations[2] +
+						possibleOperations[2] +
+						numCombinations[3] +
+						")",
+					numCombinations[0] +
+						possibleOperations[0] +
+						numCombinations[1] +
+						possibleOperations[1] +
+						"(" +
+						numCombinations[2] +
+						possibleOperations[2] +
+						numCombinations[3] +
+						")",
+					numCombinations[0] +
+						possibleOperations[0] +
+						"(" +
+						numCombinations[1] +
+						possibleOperations[1] +
+						numCombinations[2] +
+						")" +
+						possibleOperations[2] +
+						numCombinations[3],
+				];
 
-        /** loop over the calculation list */
-        for (const calculation of calculationList) {
-          /** if calculation equals the desired number */
-          if (eval(calculation) === Number(goal)) {
-            /** replace "*" and "/" with "×" and "÷" for better readability */
-            const solution = calculation
-              .replaceAll("*", "×")
-              .replaceAll("/", "÷");
+				/** loop over the calculation list */
+				for (const calculation of calculationList) {
+					/** if calculation equals the desired number */
+					if (eval(calculation) === Number(goal)) {
+						/** replace "*" and "/" with "×" and "÷" for better readability */
+						const solution = calculation
+							.replaceAll("*", "×")
+							.replaceAll("/", "÷");
 
-            /** check if calculation is not already in result */
-            if (!result.includes(solution))
-              /** push the calculation to the result array */
-              result.push(solution);
-          }
-        }
-      }
-    }
+						/** check if calculation is not already in result */
+						if (!result.includes(solution))
+							/** push the calculation to the result array */
+							result.push(solution);
+					}
+				}
+			}
+		}
 
-    /** log the number of solutions */
-    // console.log(`There are ${result.length} solutions.`);
+		/** log the number of solutions */
+		// console.log(`There are ${result.length} solutions.`);
 
-    /** log the solutions */
-    // console.log("Solutions: ", result);
+		/** log the solutions */
+		// console.log("Solutions: ", result);
 
-    /** end measuring time */
-    let timeTaken = performance.now() - start;
+		/** end measuring time */
+		let timeTaken = performance.now() - start;
 
-    /** log time taken to calculate all these */
-    console.log("Total time taken : " + timeTaken + " milliseconds");
+		/** log time taken to calculate all these */
+		console.log("Total time taken : " + timeTaken + " milliseconds");
 
-    /** print solutions in page */
-    printResult(result);
-  } catch (error) {
-    /** log error */
-    console.error(error);
-    return false;
-  }
+		/** print solutions in page */
+		printResult(result);
+	} catch (error) {
+		/** log error */
+		console.error(error);
+		return false;
+	}
 }
 
 /**
  * input validation
  */
 function validation() {
-  if (
-    Number(numbersInput.value) &&
-    Number(goalInput.value) &&
-    numbersInput.value.length === 4 &&
-    goalInput.value.length <= 4
-  ) {
-    findSolutionsButton.disabled = false;
-  } else {
-    findSolutionsButton.disabled = true;
-  }
+	if (
+		Number(numbersInput.value) &&
+		Number(goalInput.value) &&
+		numbersInput.value.length === 4 &&
+		goalInput.value.length <= 4
+	) {
+		findSolutionsButton.disabled = false;
+	} else {
+		findSolutionsButton.disabled = true;
+	}
 }
 
 /**
@@ -207,10 +207,10 @@ function validation() {
  * @param {object} solutions - an array of all the solutions
  */
 function printResult(solutions) {
-  answersDiv.innerHTML = "";
-  numberOfSolutions.textContent = `(${solutions.length})`;
+	answersDiv.innerHTML = "";
+	numberOfSolutions.textContent = `(${solutions.length})`;
 
-  for (const solution of solutions) {
-    answersDiv.innerHTML += `<p>${solution}</p>`;
-  }
+	for (const solution of solutions) {
+		answersDiv.innerHTML += `<p>${solution}</p>`;
+	}
 }
